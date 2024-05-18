f054b08d4b423613142b3ec4929b14164523dfab
Dustin L <lawson.dustin12@gmail.com>
2024-05-18 13:44:50 -0500

    added promise and button to dom

--- public/main.js
+++ public/main.js
@@ -3,5 +3,41 @@
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
-// USE WITH FIREBASE AUTH
-// ViewDirectorBasedOnUserAuthStatus();
+const jokeEndpoint = 'https://v2.jokeapi.dev/joke/Any';
+
+const getRequest = () => new Promise((resolve, reject) => {
+  fetch(jokeEndpoint, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const htmlStructure = () => {
+  document.querySelector('#app').innerHTML = `
+  <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Joke Generator</h5>
+    <p id="joke-setup"></p>
+    <button class="btn btn-warning" id="get-joke">GET JOKE</button>
+  </div>
+</div>
+   
+   `;
+};
+
+const events = () => {
+  document.querySelector('#get-joke').addEventListener('click', () => {
+    getRequest().then(console.warn);
+  });
+};
+
+const startApp = () => {
+  htmlStructure();
+  events();
+};
+startApp();
